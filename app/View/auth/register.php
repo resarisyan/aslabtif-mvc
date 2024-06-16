fb3b367853fe894246746462f0027892634c9c46
resarisyan <resarisyan77@gmail.com>
2024-06-16 14:13:59 +0700

    feat: add auth view

---
+++ app/View/auth/register.php
@@ -0,0 +1,27 @@
+<?php require_once(__DIR__ . "/../layouts/auth/header.php"); ?>
+<div class="col-lg-6">
+    <div class="p-5">
+        <div class="text-center">
+            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+        </div>
+        <form class="user">
+            <div class="form-group">
+                <input type="name" class="form-control form-control-user" id="name" placeholder="Enter Name..." name="name">
+            </div>
+            <div class="form-group">
+                <input type="username" class="form-control form-control-user" id="username" placeholder="Enter Username..." name="username">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-user btn-block">
+                Register
+            </button>
+        </form>
+        <hr>
+        <div class="text-center">
+            <a class="small" href="<?= BASEURL . '/register' ?>">Create an Account!</a>
+        </div>
+    </div>
+</div>
+<?php require_once(__DIR__ . "/../layouts/auth/footer.php"); ?>
---
+++ app/View/layouts/auth/footer.php
@@ -0,0 +1,23 @@
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="<?= BASEURL ?>/template/vendor/jquery/jquery.min.js"></script>
+<script src="<?= BASEURL ?>/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= BASEURL ?>/template/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= BASEURL ?>/template/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
